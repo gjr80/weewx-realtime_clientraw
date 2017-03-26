a04488dcc415a6b06e035d3db247ab14d4d0a6d5
gjr80 <gjroderick@gmail.com>
2017-03-26 15:02:01 +1000

    not sure why this do not commit

--- bin/user/rtcr.py
+++ bin/user/rtcr.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see http://www.gnu.org/licenses/.
 #
-# Version: 0.2.0                                        Date: 19 March 2017
 # Version: 0.2.1                                        Date: ?? March 2017
 #
 # Revision History
