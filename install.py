76e143ef37445fada902df8c2778cb570422c336
gjr80 <gjroderick@gmail.com>
2023-08-30 14:27:45 +1000

    update changelog and installer to v0.3.6

--- install.py
+++ install.py
@@ -10,9 +10,23 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
                      Installer for Realtime Clientraw
 
-Version: 0.2.3                                        Date: 9 March 2020
+Version: 0.3.6                                        Date: 24 March 2023
 
 Revision History
+    24 March 2023       v0.3.6
+        -   config for installer is now derived from a multiline string for
+            easy modification of config settings and to allow embedded comments
+            in installed config stanzas
+    22 January 2023     v0.3.5
+        - no change, bump version only
+    3 April 2022        v0.3.4
+        - no change, bump version only
+    7 February 2022     v0.3.3
+        - no change, bump version only
+    25 November 2021    v0.3.2
+        - no change, bump version only
+    13 May 2021         v0.3.0
+        - no change, bump version only
     9 March 2020        v0.2.3
         - no change, bump version only
     1 March 2020        v0.2.2
@@ -31,8 +45,53 @@ import weewx
 from distutils.version import StrictVersion
 from setup import ExtensionInstaller
 
-REQUIRED_VERSION = "3.4.0"
-RTCR_VERSION = "0.2.3"
+REQUIRED_VERSION = "4.5.0"
+RTCR_VERSION = "0.3.6"
+
+# Multi-line config string, makes it easier to include comments. Needs to be
+# explicitly set as unicode or python2 StringIO complains.
+rtcr_config = u"""
+[RealtimeClientraw]
+
+    # Path to clientraw.txt. Can be an absolute or relative path. Relative
+    # paths are relative to ????. Optional, default setting is to use
+    # ????.
+    rtcr_path = /home/weewx/public_html
+
+    # If using an external website, configure remote_server_url to point to 
+    # the post_clientraw.php script on your website like:
+    #   remote_server_url = http://your.website.com/post_clientraw.php
+    #
+    # To disable or use the webserver on this system, leave the entry 
+    # commented out or blank.
+    # remote_server_url = http://your.website.com/post_clientraw.php
+
+    # min_interval sets the minimum clientraw.txt generation interval. 
+    # 10 seconds is recommended for all Saratoga template users. Default 
+    # is 0 seconds.
+    min_interval = 10
+
+    # Python date-time format strings. Format string codes as per 
+    # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+
+    # Date format. Recommended entries are:
+    #   date_format = %-m/%-d/%Y  # recommended for USA users
+    #   date_format = %-d/%-m/%Y  # recommended for non-USA users
+    date_format = %-d/%-m/%Y
+
+    # Long format times (HMS). Recommended entries are:
+    #   long_time_format = %-I:%M:%S_%p  # recommended for USA users
+    #   long_time_format = %H:%M:%S  # recommended for non-USA users 
+    long_time_format = %H:%M:%S
+
+    # Short format times (HM). Recommended entries are:
+    #   short_time_format = %-I:%M_%p  # recommended for USA users
+    #   short_time_format = %H:%M  # recommended for non-USA users
+    short_time_format = %H:%M
+"""
+
+# obtain our config string as a configobj dict
+rtcr_dict = configobj.ConfigObj(StringIO(rtcr_config))
 
 
 def loader():
@@ -42,21 +101,17 @@ def loader():
 class RtcrInstaller(ExtensionInstaller):
     def __init__(self):
         if StrictVersion(weewx.__version__) < StrictVersion(REQUIRED_VERSION):
-            msg = "%s requires weeWX %s or greater, found %s" % ('Rtcr ' + RTCR_VERSION,
+            msg = "%s requires WeeWX %s or greater, found %s" % ('Rtcr ' + RTCR_VERSION,
                                                                  REQUIRED_VERSION,
                                                                  weewx.__version__)
             raise weewx.UnsupportedFeature(msg)
         super(RtcrInstaller, self).__init__(
             version=RTCR_VERSION,
-            name='Rtcr',
+            name='RTCR',
             description='WeeWX support for near realtime generation of a limited clientraw.txt.',
             author="Gary Roderick",
             author_email="gjroderick@gmail.com",
             report_services=['user.rtcr.RealtimeClientraw'],
-            config={
-                'RealtimeClientraw': {
-                    'rtcr_path': '/home/weewx/public_html'
-                }
-            },
+            config=rtcr_dict,
             files=[('bin/user', ['bin/user/rtcr.py'])]
         )
